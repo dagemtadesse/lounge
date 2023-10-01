d5eb049016a539996df4929d1d4c5589868090e7
Dagem Tadesse <dagem.seyfu@gmail.com>
2023-10-01 14:50:08 +0300

    transformer

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -4,14 +4,17 @@ import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc } from "./client";
 import { httpBatchLink } from "@trpc/react-query";
+import SuperJSON from "superjson";
 
 let URL = "http://localhost:3000";
-if (process.env.NEXT_PUBLIC_VERCEL_URL) URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+if (process.env.NEXT_PUBLIC_VERCEL_URL)
+  URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export default function TRPCProvider({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   const [trpcClient] = useState(() =>
     trpc.createClient({
+      transformer: SuperJSON,
       links: [httpBatchLink({ url: `${URL}/api/trpc` })],
     })
   );
